dabdbf88591da80af11835ac84494ae54d0abf38
Carlos Albornoz <carlos.alb.rom@gmail.com>
2022-01-19 09:12:35 -0400

    Admin created

---
+++ src/app/pages/admin/admin-color/admin-color.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-color',
+  templateUrl: './admin-color.component.html',
+  styleUrls: ['./admin-color.component.scss']
+})
+export class AdminColorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { LoginComponent } from './login/login.component';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { AdminArticleComponent } from './admin-article/admin-article.component';
+import { AdminColorComponent } from './admin-color/admin-color.component';
+import { AdminTagComponent } from './admin-tag/admin-tag.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    AdminHomeComponent,
+    AdminArticleComponent,
+    AdminColorComponent,
+    AdminTagComponent,
+    AdminLoginComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
